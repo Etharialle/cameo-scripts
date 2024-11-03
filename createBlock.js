1c3a67c36ab6386c23a7d3deef3424e9d63d2a85
Christopher Adams <christopher.m.adams2@gmail.com>
2024-11-03 06:44:16 -0800

    resolve getName error

--- createBlock.js
+++ createBlock.js
@@ -1,4 +1,4 @@
-// Sample JSON object representing elements (blocks) to create
+// Sample JSON data representing elements (blocks) to create
 var jsonData = '[{"name": "Block1"}, {"name": "Block2"}, {"name": "Block3"}]';
 
 // Parse the JSON data
@@ -9,28 +9,34 @@ var project = com.nomagic.magicdraw.core.Application.getInstance().getProject();
 var elementsFactory = project.getElementsFactory();
 var model = project.getModel(); // Get the root model element
 
-// Find the owner element by name
+// Function to find the owner element by name
 function findElementByName(name) {
     var ownedElements = model.getOwnedElement().toArray();
     for (var i = 0; i < ownedElements.length; i++) {
-        if (ownedElements[i].getName() === name) {
-            return ownedElements[i];
+        var element = ownedElements[i];
+        if (element instanceof com.nomagic.uml2.ext.magicdraw.classes.mdkernel.NamedElement &&
+            element.getName() === name) {
+            return element;
         }
     }
-    return null; // Return null if not found
+    return null; // Return null if the element is not found
 }
 
 // Function to create a new block under the specified owner
 function createBlock(name, owner) {
-    var block = elementsFactory.createBlockInstance(); // Create a new Block instance
-    block.setName(name); // Set the name of the block
-    owner.addOwnedElement(block); // Add the block as owned by the specified owner
-    print("Created new block: " + name + " under owner: " + owner.getName());
+    var block = elementsFactory.createClassInstance(); // Using createClassInstance as a substitute for Block
+    if (block instanceof com.nomagic.uml2.ext.magicdraw.classes.mdkernel.NamedElement) {
+        block.setName(name); // Set the block's name
+        owner.addOwnedElement(block); // Add the block as owned by the specified owner
+        print("Created new block: " + name + " under owner: " + owner.getName());
+    } else {
+        print("Created element is not a NamedElement and cannot have a name.");
+    }
     return block;
 }
 
 // Main script logic
-var ownerName = "Aures Topics"; // The name of the owner
+var ownerName = "Scripts"; // The name of the owner
 var ownerElement = findElementByName(ownerName); // Find the owner element
 
 if (ownerElement === null) {
